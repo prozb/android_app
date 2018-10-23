074e7bc7fcb498f82d93a5e364fd147f2c7f3339
= <rozbich15@gmail.com>
2018-10-23 22:56:46 +0200

    added some properties

--- app/src/main/java/de/thm/ap/ap_przb86_u1/RecordsActivity.java
+++ app/src/main/java/de/thm/ap/ap_przb86_u1/RecordsActivity.java
@@ -2,15 +2,18 @@ package de.thm.ap.ap_przb86_u1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RecordsActivity extends AppCompatActivity {
+    private ListView recordListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_records);
+
+        recordListView = findViewById(R.id.records_list);
+        recordListView.setEmptyView(findViewById(R.id.records_list_empty));
     }
-
-
 }
