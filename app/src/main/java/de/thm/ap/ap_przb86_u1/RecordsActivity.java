133923167a09b7c8832f271f6d87ec7678ffcd23
= <rozbich15@gmail.com>
2018-10-23 23:23:25 +0200

    created menu buttons

--- app/src/main/java/de/thm/ap/ap_przb86_u1/RecordsActivity.java
+++ app/src/main/java/de/thm/ap/ap_przb86_u1/RecordsActivity.java
@@ -2,7 +2,13 @@ package de.thm.ap.ap_przb86_u1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.List;
+
+import de.thm.ap.ap_przb86_u1.model.Record;
 
 public class RecordsActivity extends AppCompatActivity {
     private ListView recordListView;
@@ -16,4 +22,22 @@ public class RecordsActivity extends AppCompatActivity {
         recordListView = findViewById(R.id.records_list);
         recordListView.setEmptyView(findViewById(R.id.records_list_empty));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.records, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//        List<Record> records = new RecordDAO(this).findAll();
+//
+//        ArrayAdapter<Record> adapter = new ArrayAdapter<>(this,
+//                    android.R.layout.simple_list_item_1, records);
+//
+//        recordListView.setAdapter(adapter);
+     }
 }
---
+++ app/src/main/java/de/thm/ap/ap_przb86_u1/model/Record.java
@@ -0,0 +1,4 @@
+package de.thm.ap.ap_przb86_u1.model;
+
+public class Record {
+}
