fb5dacf0778e9b810116d185a85948dea2de4a8d
= <rozbich15@gmail.com>
2018-10-29 02:01:27 +0100

    created choosing menu

--- app/src/main/java/de/thm/ap/ap_przb86_u1/RecordsActivity.java
+++ app/src/main/java/de/thm/ap/ap_przb86_u1/RecordsActivity.java
@@ -5,16 +5,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import de.thm.ap.ap_przb86_u1.model.Record;
@@ -39,6 +37,34 @@ public class RecordsActivity extends AppCompatActivity {
             i.putExtra("position", position);
 
             startActivity(i);
+        });
+        recordListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        recordListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                Toast.makeText(RecordsActivity.this, "chosed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) { // Inflate the menu for the CAB
+                getMenuInflater().inflate(R.menu.choose_menu, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
         });
     }
 
