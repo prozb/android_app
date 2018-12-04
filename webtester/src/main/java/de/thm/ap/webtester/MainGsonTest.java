f7ddfa818b374190941eafd1b310cc71d1137998
= <rozbich15@gmail.com>
2018-12-04 09:03:33 +0100

    closes #20

--- webtester/src/main/java/de/thm/ap/webtester/MainGsonTest.java
+++ webtester/src/main/java/de/thm/ap/webtester/MainGsonTest.java
@@ -7,13 +7,25 @@ import android.support.annotation.RequiresApi;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainGsonTest {
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("modules.json");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write("hello pidors");
+        writer.flush();
+        writer.close();
+
+        System.out.println(file.getAbsolutePath());
         List<TaskGson> list = new ArrayList<>();
         for(int i = 0; i < 20; i++){
             list.add(new TaskGson(i, "Test1", "Test2",
